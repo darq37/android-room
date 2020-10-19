a9caaa2543fc8d80dd1b4fdc66011371729a8ac6
darq37 <dzmudziak@gmail.com>
2020-10-19 10:36:42 +0200

    Add templates for activities.

--- app/src/main/java/com/darq37/android_room/MainActivity.java
+++ app/src/main/java/com/darq37/android_room/MainActivity.java
@@ -3,14 +3,19 @@ package com.darq37.android_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.darq37.android_room.activities.login.ui.login.LoginActivity;
 import com.darq37.android_room.database.RoomConstant;
 import com.darq37.android_room.database.room.AppDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private static Context context;
     private AppDatabase appDatabase;
+    private Button button;
 
     public static Context getContext() {
         return context;
@@ -22,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         context = getApplicationContext();
         appDatabase = RoomConstant.getInstance(this);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnLogin);
+    }
+
+    public void goToLoginAtivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/com/darq37/android_room/activities/user/UserActivity.java
@@ -0,0 +1,16 @@
+package com.darq37.android_room.activities.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.darq37.android_room.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
