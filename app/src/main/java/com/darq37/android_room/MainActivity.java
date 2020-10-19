8d355bddfcc24a225cc80ba3065ae29dbcd0bb24
darq37 <dzmudziak@gmail.com>
2020-10-19 11:03:20 +0200

    Design main activity

--- app/src/main/java/com/darq37/android_room/MainActivity.java
+++ app/src/main/java/com/darq37/android_room/MainActivity.java
@@ -2,6 +2,7 @@ package com.darq37.android_room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,9 +14,9 @@ import com.darq37.android_room.database.RoomConstant;
 import com.darq37.android_room.database.room.AppDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    @SuppressLint("StaticFieldLeak")
     private static Context context;
     private AppDatabase appDatabase;
-    private Button button;
 
     public static Context getContext() {
         return context;
@@ -28,12 +29,22 @@ public class MainActivity extends AppCompatActivity {
         appDatabase = RoomConstant.getInstance(this);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btnLogin);
+        Button exitButton = findViewById(R.id.exitButton);
+        Button loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(this::toLoginActivity);
+        exitButton.setOnClickListener(this::exitApp);
     }
 
-    public void goToLoginAtivity(View view) {
+    public void toLoginActivity(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void exitApp(View view){
+        finish();
+        System.exit(0);
     }
 
 
