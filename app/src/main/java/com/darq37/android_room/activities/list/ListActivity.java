03962a0e8a196f1089441b316792cab247a9debf
darq37 <dzmudziak@gmail.com>
2020-10-26 10:52:35 +0100

    Add basic basic functionality to create new list of products.

--- app/src/main/java/com/darq37/android_room/activities/list/ListActivity.java
+++ app/src/main/java/com/darq37/android_room/activities/list/ListActivity.java
@@ -8,23 +8,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.darq37.android_room.MainActivity;
 import com.darq37.android_room.R;
 import com.darq37.android_room.activities.product.ProductActivity;
 import com.darq37.android_room.activities.product.ProductAdapter;
 import com.darq37.android_room.database.RoomConstant;
+import com.darq37.android_room.entity.ShoppingList;
+import com.darq37.android_room.entity.User;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Date;
 
 
 public class ListActivity extends AppCompatActivity {
+    private EditText listName;
+    private String userLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        listName = findViewById(R.id.new_list_name);
+
         Button addProductButton = findViewById(R.id.goToProductActivity);
+        Button addListButton = findViewById(R.id.new_list_button);
 
         RecyclerView productRV = findViewById(R.id.productListView);
         RecyclerView shoppingListRV = findViewById(R.id.shoppingListsView);
@@ -42,10 +53,28 @@ public class ListActivity extends AppCompatActivity {
         shoppingListRV.setAdapter(shoppingListAdapter);
 
         addProductButton.setOnClickListener(this::goToProductActivity);
+        addListButton.setOnClickListener(this::addNewList);
     }
 
     public void goToProductActivity(View view) {
         Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
         startActivity(intent);
+    }
+
+    public void addNewList(View view) {
+        String newListName = listName.getText().toString();
+        ShoppingList shoppingList = new ShoppingList();
+        User owner = RoomConstant.getInstance(this).userDao().getByIdSync(userLogin);
+        shoppingList.setName(newListName);
+        shoppingList.setOwner(owner);
+        shoppingList.setCreationDate(new Date());
+        shoppingList.setModificationDate(new Date());
+        //TODO set User for new List
+        RoomConstant.getInstance(this).shoppingListDao().insertSync(shoppingList);
+        listName.setText("");
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
     }
 }
