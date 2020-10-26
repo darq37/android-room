a317be5058aeae731a00fb5cf254c268e797721c
darq37 <dzmudziak@gmail.com>
2020-10-26 09:08:33 +0100

    Change minimum SDK version to 29. Add new activity to create new Shopping list.

---
+++ app/src/main/java/com/darq37/android_room/activities/newList/NewListActivity.java
@@ -0,0 +1,16 @@
+package com.darq37.android_room.activities.newList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.darq37.android_room.R;
+
+public class NewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_list);
+    }
+}
