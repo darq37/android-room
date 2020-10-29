24272d5c24e4baf8d78aa85afcb39b30a2b863bb
darq37 <dzmudziak@gmail.com>
2020-10-29 13:52:26 +0100

    Add onClickListener to SharedListAdapter itemViewHolder to start ListDetailsActivity with accurate list_id.

--- app/src/main/java/com/darq37/android_room/adapters/SharedListAdapter.java
+++ app/src/main/java/com/darq37/android_room/adapters/SharedListAdapter.java
@@ -1,5 +1,7 @@
 package com.darq37.android_room.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.darq37.android_room.R;
+import com.darq37.android_room.activities.listDetails.ListDetailsActivity;
 import com.darq37.android_room.entity.SharedList;
 
 import java.util.List;
@@ -33,6 +36,12 @@ public class SharedListAdapter extends RecyclerView.Adapter<SharedListAdapter.Sh
         SharedList sharedList = sharedListList.get(position);
         holder.name.setText(sharedList.getShoppingList().getName());
         holder.from.setText(sharedList.getShoppingList().getOwner().getDisplayName());
+        holder.itemView.setOnClickListener(v -> {
+            Context context = v.getContext();
+            Intent intent = new Intent(context, ListDetailsActivity.class);
+            intent.putExtra("list_id", sharedList.getShoppingList().getId());
+            context.startActivity(intent);
+        });
     }
 
     @Override
