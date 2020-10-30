aada8a25c014b4cc971aad9487556dcf9ca98922
darq37 <dzmudziak@gmail.com>
2020-10-30 16:54:12 +0100

    Add async insert to ProductActivity.

--- app/src/main/java/com/darq37/android_room/activities/product/ProductActivity.java
+++ app/src/main/java/com/darq37/android_room/activities/product/ProductActivity.java
@@ -1,44 +1,65 @@
 package com.darq37.android_room.activities.product;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.darq37.android_room.R;
 import com.darq37.android_room.adapters.ProductAdapter;
 import com.darq37.android_room.database.RoomConstant;
+import com.darq37.android_room.database.dao.ProductDao;
 import com.darq37.android_room.entity.Product;
+
+import java.util.Collections;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 
 public class ProductActivity extends AppCompatActivity {
 
     private EditText productName;
     private EditText productDescription;
+    private Button addProductButton;
+    private ProductDao productDao;
+    private ProductAdapter productAdapter;
+    private RecyclerView productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
-        Button addProductButton = findViewById(R.id.addProductButton);
-        productName = findViewById(R.id.productName);
-        productDescription = findViewById(R.id.productDescription);
+        productDao = RoomConstant.getInstance(this).productDao();
+        productAdapter = new ProductAdapter(Collections.emptyList());
+
+        initializeViews();
+
+        productDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess(products -> {
+                    productAdapter.setProductList(products);
+                    productList.setHasFixedSize(true);
+                    productList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    productList.setAdapter(productAdapter);
+                })
+                .subscribe();
 
         addProductButton.setOnClickListener(this::addProduct);
+    }
 
-        RecyclerView productList = findViewById(R.id.productListView);
-        productList.setHasFixedSize(true);
-        productList.setLayoutManager(new LinearLayoutManager(this));
-
-        ProductAdapter productAdapter =  new ProductAdapter(RoomConstant.getInstance(this).productDao().getAllSync());
-        productList.setAdapter(productAdapter);
-
+    private void initializeViews() {
+        productList = findViewById(R.id.productListView);
+        addProductButton = findViewById(R.id.addProductButton);
+        productName = findViewById(R.id.productName);
+        productDescription = findViewById(R.id.productDescription);
     }
 
     public void addProduct(View view) {
@@ -47,10 +68,15 @@ public class ProductActivity extends AppCompatActivity {
         Product p = new Product();
         p.setName(name);
         p.setDescription(description);
-        RoomConstant.getInstance(this).productDao().insertSync(p);
-        Toast.makeText(getApplicationContext(), "Product saved", Toast.LENGTH_LONG).show();
-        productName.setText("");
-        productDescription.setText("");
+        productDao.insert(p)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess(response -> {
+                    productAdapter.notifyDataSetChanged();
+                    Toast.makeText(getApplicationContext(), "Product saved", Toast.LENGTH_LONG).show();
+                    productName.setText("");
+                    productDescription.setText("");
+                })
+                .subscribe();
     }
-
 }
