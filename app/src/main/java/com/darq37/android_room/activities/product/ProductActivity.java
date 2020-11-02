8ddc3d6e80aa4e277fd1c2b3e5cc53b5810fbc52
darq37 <dzmudziak@gmail.com>
2020-11-02 12:22:23 +0100

    Add pull-to-refresh to ProductActivity's RecyclerView.

--- app/src/main/java/com/darq37/android_room/activities/product/ProductActivity.java
+++ app/src/main/java/com/darq37/android_room/activities/product/ProductActivity.java
@@ -1,6 +1,7 @@
 package com.darq37.android_room.activities.product;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.darq37.android_room.R;
 import com.darq37.android_room.adapters.ProductAdapter;
@@ -30,6 +32,7 @@ public class ProductActivity extends AppCompatActivity {
     private ProductDao productDao;
     private ProductAdapter productAdapter;
     private RecyclerView productList;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,6 @@ public class ProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product);
 
         productDao = RoomConstant.getInstance(this).productDao();
-        productAdapter = new ProductAdapter(Collections.emptyList());
 
         initializeViews();
 
@@ -53,6 +55,24 @@ public class ProductActivity extends AppCompatActivity {
                 .subscribe();
 
         addProductButton.setOnClickListener(this::addProduct);
+        swipeRefreshLayout.setOnRefreshListener(this::refreshData);
+    }
+
+    private void refreshData() {
+        final Handler handler = new Handler();
+        productDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess(products -> {
+                    productAdapter.setProductList(products);
+                    Toast.makeText(this, "Data refreshed.", Toast.LENGTH_SHORT).show();
+                    swipeRefreshLayout.setRefreshing(false);
+                }).doOnError(error -> handler.postDelayed(() -> {
+            Toast.makeText(this, "Cannot refresh the data.", Toast.LENGTH_SHORT).show();
+            if (swipeRefreshLayout.isRefreshing()) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1500)).subscribe();
     }
 
     private void initializeViews() {
@@ -60,6 +80,8 @@ public class ProductActivity extends AppCompatActivity {
         addProductButton = findViewById(R.id.addProductButton);
         productName = findViewById(R.id.productName);
         productDescription = findViewById(R.id.productDescription);
+        swipeRefreshLayout = findViewById(R.id.product_swipe_refresh);
+        productAdapter = new ProductAdapter(Collections.emptyList());
     }
 
     public void addProduct(View view) {
