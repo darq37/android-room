0c851d43ac112ce9caeb63e16e11b1845b521ea2
darq37 <dzmudziak@gmail.com>
2020-11-10 10:20:25 +0100

    Use ApiService for list sharing.

--- app/src/main/java/com/darq37/android_room/database/RoomConstant.java
+++ app/src/main/java/com/darq37/android_room/database/RoomConstant.java
@@ -10,16 +10,6 @@ public class RoomConstant {
     public static AppDatabase buildDatabase(Context context) {
         return Room.databaseBuilder(context, AppDatabase.class, "app_db")
                 .fallbackToDestructiveMigration()
-                /*.addCallback(new RoomDatabase.Callback() {
-                    @Override
-                    public void onCreate(@NonNull SupportSQLiteDatabase db) {
-                        db.execSQL("INSERT INTO users(login, display_name, password, user_creation_date, user_modification_date) VALUES ('login', 'Dominik', 'password', 776995200000, 776995200000)");
-                        db.execSQL("INSERT INTO products(product_name, product_description) VALUES ('Duck', 'Regular rubber duck')");
-                        db.execSQL("INSERT INTO products(product_name, product_description) VALUES ('Carrot', 'Delicious carrot')");
-                        db.execSQL("INSERT INTO products(product_name, product_description) VALUES ('Pistol', 'Deadly weapon')");
-                        db.execSQL("INSERT INTO products(product_name, product_description) VALUES ('Toilet paper', 'Extra soft toilet paper')");
-                    }
-                })*/
                 .build();
 
     }
